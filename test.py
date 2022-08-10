bfce3cb61ff0f3c25a2f53f85fbb86824e535c1b
zhanghao2 <zhanghao2@sg.cambricon.com>
2022-08-10 11:33:10 +0800

    add py_iou and the comparision

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import numpy as np
 from zh_bev_iou.bev_iou_utils import boxes_bev_giou_cpu
 from zh_bev_iou.bev_iou_utils import boxes_bev_iou_cpu
 
+from py_iou import iou2d, giou2d, iouMatrix
 
 # a=torch.Tensor([[1,1,1,
 #                  2,2,2,
@@ -14,15 +15,15 @@ from zh_bev_iou.bev_iou_utils import boxes_bev_iou_cpu
 #                  2.1,2,2,
 #                  0.1]])
 
-a = torch.rand((5, 7))
-b = torch.rand((5, 7))
+a = torch.rand((50, 7))
+b = torch.rand((50, 7))
                  
 s = time.time()                
 for i in range(1):
     iou_mat = boxes_bev_iou_cpu(a, b)
 e = time.time()  
 print(" iou cpu using: %.2fs"%(e - s))
-print(iou_mat)
+# print(iou_mat)
 
 
 s = time.time()                
@@ -30,4 +31,22 @@ for i in range(1):
     giou_mat = boxes_bev_giou_cpu(a, b)
 e = time.time()  
 print("giou cpu using: %.2fs"%(e - s))
-print(giou_mat)
+# print(giou_mat)
+
+
+np_a, np_b = a.numpy(), b.numpy()
+
+s = time.time()                
+for i in range(1):
+    iou_mat_py = iouMatrix(np_a, np_b, iou2d)
+e = time.time()  
+print("py iou using: %.2fs"%(e - s))
+# print(iou_mat_py)
+
+
+s = time.time()                
+for i in range(1):
+    giou_mat_py = iouMatrix(np_a, np_b, giou2d)
+e = time.time()  
+print("py giou using: %.2fs"%(e - s))
+# print(giou_mat_py)
--- test2.py
+++ test2.py
@@ -5,24 +5,22 @@ import numpy as np
 from zh_bev_iou.bev_iou_utils import boxes_bev_giou_cpu
 from zh_bev_iou.bev_iou_utils import boxes_bev_iou_cpu
 
-
-a=torch.Tensor([[1,  1,  1,
-                 4,  2,  2,
-                 0
-                ]])
-
-b=torch.Tensor([[5,  1,  1,
-                 4,  2,  2,
-                 0
-                ]])
+from py_iou import iou2d, giou2d
 
 
+
+boxa = np.array([10., 20., 30, 4, 3, 1, 0]).astype(np.float32)
+boxb = np.array([10., 20., 31, 4.2, 3, 1, np.pi/2.0]).astype(np.float32)
+
+a=torch.from_numpy(boxa).reshape(-1, 7)
+b=torch.from_numpy(boxb).reshape(-1, 7)
 
 iou_mat = boxes_bev_iou_cpu(a, b)
-print(iou_mat)
-
-
+print("iou = ", iou_mat)
 
 giou_mat = boxes_bev_giou_cpu(a, b)
-print(giou_mat)
+print("giou = ", giou_mat)
 
+
+print("py iou = ", iou2d(boxa, boxb))
+print("py giou = ", giou2d(boxa, boxb))
